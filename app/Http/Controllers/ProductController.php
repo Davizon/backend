1e598e1ddfe410d9a5311a5aca9f5f9c9ed6dd4d
Davizon <davison1105@gmail.com>
2019-12-10 11:47:41 -0400

    Inicio de Creacion de la api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::all();
+        return response()->json($products->load('categories', 'options','vendor_products','states','features'));
+    }
 }
