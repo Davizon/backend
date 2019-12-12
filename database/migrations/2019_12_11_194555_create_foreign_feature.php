c5c08a934cc98350be1db49376d81d42718784ed
Davizon <davison1105@gmail.com>
2019-12-12 11:33:51 -0400

    update foreing feature

--- database/migrations/2019_12_11_194555_create_foreign_feature.php
+++ database/migrations/2019_12_11_194555_create_foreign_feature.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateForeningFeaure extends Migration
+class CreateForeignFeature extends Migration
 {
     /**
      * Run the migrations.
@@ -14,9 +14,8 @@ class CreateForeningFeaure extends Migration
     public function up()
     {
         Schema::table('product_features', function (Blueprint $table) {
-//            $table->foreign('feature_value_id')->references('id')->on('product_feature_values')->onDelete('cascade')->onUpdate('cascade');
-//            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
-//            $table->foreign('feature_value_description_id')->references('id')->on('feature_value_descriptions')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('product_feature_value_id')->references('id')->on('product_feature_values')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
