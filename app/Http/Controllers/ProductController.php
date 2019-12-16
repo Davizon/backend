edd1330acd8b3e87c442822a72c70cb7c0ae9d00
Dariel Abreu <odalisdabreu@gmail.com>
2019-12-16 09:33:37 -0400

    para descartar

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ProductFeatureValue;
 use Illuminate\Http\Request;
 use App\Product;
 
@@ -15,6 +16,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
+        $value = ProductFeatureValue::all();
         return response()->json($products->load('product_features_value'));
     }
 
